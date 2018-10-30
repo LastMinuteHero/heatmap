4457c0efe9d1ee2dbecaced7994254b077d6438c
Kai Hartmann <kai@Kais-MacBook-Pro.local>
2018-10-30 19:26:06 +0100

    Map Marker GeoJson Data from Firestore displayed in Webapp

--- frontend/assets/js/index.js
+++ frontend/assets/js/index.js
@@ -19,159 +19,27 @@ db.settings({
 });
 
 
-// Add Stuff to DB
+var jsonObject;
+var map;
 
-// db.collection("users").add({
-//     first: "Ada",
-//     last: "Lovelace",
-//     born: 1815
-// })
-// .then(function(docRef) {
-//     console.log("Document written with ID: ", docRef.id);
-// })
-// .catch(function(error) {
-//     console.error("Error adding document: ", error);
-// });a
-
-
-
-//Query
-db.collection("users").get().then((querySnapshot) => {
+//Get JSON Data, then add to map.data
+db.collection("features").get().then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
         console.log(`${doc.id} => ${doc.data()}`);
         console.log(doc.data());
+        jsonObject = doc.data();
+        console.log(jsonObject);
+        map.data.addGeoJson(jsonObject); 
     });
 });
 
 
-//Realtime Updates https://firebase.google.com/docs/firestore/query-data/listen
-// db.collection("users").doc("SF")
-//     .onSnapshot(function(doc) {
-//         console.log("Current data: ", doc.data());
-//         console.log(doc.data());
-//     });
-
-
-// !!!!! CREATE MULTIPLE DATA Points then upload to Firebase and load from there!
-
-
-var jsonObject = {
-    "type": "FeatureCollection",
-    "features": [
-        {
-            "type": "Feature",
-            "properties": {
-                "skala": 6,
-                "category": "Awesomeness",
-                "place": "Dresdner Straße 26 04317 Leipzig",
-                "time": 1348174056,
-                "url": "http://google.com",
-                "status": "aktuell",
-                "sources": "twitter"
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    12.389230728149414,
-                    51.33372202647613
-                ]
-            }
-        },
-        {
-            "type": "Feature",
-            "properties": {
-                "skala": 6,
-                "category": "Awesomeness",
-                "place": "Dresdner Straße 26 04317 Leipzig",
-                "time": 1348174056,
-                "url": "http://google.com",
-                "status": "aktuell",
-                "sources": "twitter"
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    12.397384643554688,
-                    51.34605424944661
-                ]
-            }
-        },
-        {
-            "type": "Feature",
-            "properties": {
-                "skala": 6,
-                "category": "Awesomeness",
-                "place": "Dresdner Straße 26 04317 Leipzig",
-                "time": 1348174056,
-                "url": "http://google.com",
-                "status": "aktuell",
-                "sources": "twitter"
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    12.392749786376951,
-                    51.33870893358745
-                ]
-            }
-        },
-        {
-            "type": "Feature",
-            "properties": {
-                "skala": 6,
-                "category": "Awesomeness",
-                "place": "Dresdner Straße 26 04317 Leipzig",
-                "time": 1348174056,
-                "url": "http://google.com",
-                "status": "aktuell",
-                "sources": "twitter"
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    12.398242950439453,
-                    51.34315915601202
-                ]
-            }
-        }
-    ]
-}
-
-
-
-var dd = {
-    "type": "Feature",
-    "properties": {
-        "skala": 6,
-        "category": "Awesomeness",
-        "place": "Dresdner Straße 26 04317 Leipzig",
-        "time": 1348174056,
-        "url": "http://google.com",
-        "status": "aktuell",
-        "sources": "twitter"
-    },
-    "geometry": {
-        "type": "Point",
-        "coordinates": [
-            12.404864,
-            51.337349
-        ]
-    },
-    "id": "usc000csx3"
-}
-
-
-var map;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         zoom: 13,
         center: { lat: 51.3397, lng: 12.3731 },
         mapTypeId: 'terrain'
     });
-    // Create a <script> tag and set the USGS URL as the source.
-
-
-    map.data.addGeoJson(jsonObject);
 
     map.data.setStyle(function (feature) {
         var magnitude = feature.getProperty('skala');
@@ -198,3 +66,106 @@ function eqfeed_callback(results) {
 
 
 
+
+
+
+
+
+//Realtime Updates https://firebase.google.com/docs/firestore/query-data/listen
+// db.collection("users").doc("SF")
+//     .onSnapshot(function(doc) {
+//         console.log("Current data: ", doc.data());
+//         console.log(doc.data());
+//     });
+
+
+
+// Add Stuff to DB
+
+// db.collection("features").add({
+//     "type": "FeatureCollection",
+//     "features": [
+//         {
+//             "type": "Feature",
+//             "properties": {
+//                 "skala": 6,
+//                 "category": "Awesomeness",
+//                 "place": "Dresdner Straße 26 04317 Leipzig",
+//                 "time": 1348174056,
+//                 "url": "http://google.com",
+//                 "status": "aktuell",
+//                 "sources": "twitter"
+//             },
+//             "geometry": {
+//                 "type": "Point",
+//                 "coordinates": [
+//                     12.389230728149414,
+//                     51.33372202647613
+//                 ]
+//             }
+//         },
+//         {
+//             "type": "Feature",
+//             "properties": {
+//                 "skala": 6,
+//                 "category": "Awesomeness",
+//                 "place": "Dresdner Straße 26 04317 Leipzig",
+//                 "time": 1348174056,
+//                 "url": "http://google.com",
+//                 "status": "aktuell",
+//                 "sources": "twitter"
+//             },
+//             "geometry": {
+//                 "type": "Point",
+//                 "coordinates": [
+//                     12.397384643554688,
+//                     51.34605424944661
+//                 ]
+//             }
+//         },
+//         {
+//             "type": "Feature",
+//             "properties": {
+//                 "skala": 6,
+//                 "category": "Awesomeness",
+//                 "place": "Dresdner Straße 26 04317 Leipzig",
+//                 "time": 1348174056,
+//                 "url": "http://google.com",
+//                 "status": "aktuell",
+//                 "sources": "twitter"
+//             },
+//             "geometry": {
+//                 "type": "Point",
+//                 "coordinates": [
+//                     12.392749786376951,
+//                     51.33870893358745
+//                 ]
+//             }
+//         },
+//         {
+//             "type": "Feature",
+//             "properties": {
+//                 "skala": 6,
+//                 "category": "Awesomeness",
+//                 "place": "Dresdner Straße 26 04317 Leipzig",
+//                 "time": 1348174056,
+//                 "url": "http://google.com",
+//                 "status": "aktuell",
+//                 "sources": "twitter"
+//             },
+//             "geometry": {
+//                 "type": "Point",
+//                 "coordinates": [
+//                     12.398242950439453,
+//                     51.34315915601202
+//                 ]
+//             }
+//         }
+//     ]
+// })
+// .then(function(docRef) {
+//     console.log("Document written with ID: ", docRef.id);
+// })
+// .catch(function(error) {
+//     console.error("Error adding document: ", error);
+// });
