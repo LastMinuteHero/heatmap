2cbf578938ef477731f2e2502a5f15cba025fb04
Kai Hartmann <kai@Kais-MacBook-Pro.local>
2018-11-08 15:12:26 +0100

    Info Windows in Map Markers with correct data displayed, adjusted index.html

--- frontend/assets/js/index.js
+++ frontend/assets/js/index.js
@@ -5,7 +5,7 @@
 
 
 
-
+//Init Firebase
 firebase.initializeApp({
     apiKey: 'AIzaSyB2Cr_PGN8dKQmj9_om6z-Ikn_AWEFp_gE',
     authDomain: 'heatmap-f5281.firebaseapp.com',
@@ -20,27 +20,58 @@ db.settings({
     timestampsInSnapshots: true
 });
 
-
 var jsonObject;
 var map;
 
 //Get JSON Data, then add to map.data
 db.collection("features").get().then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => ${doc.data()}`);
-        console.log(doc.data());
+        //console.log(`${doc.id} => ${doc.data()}`);
         jsonObject = doc.data();
-        console.log(jsonObject);
-        map.data.addGeoJson(jsonObject); 
+        map.data.addGeoJson(jsonObject); //Data is added to Gmap and then setStyle in initMap--> Circle with magnitude
     });
 });
 
-
+//Init GMap
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         zoom: 13,
         center: { lat: 51.3397, lng: 12.3731 },
         mapTypeId: 'terrain'
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    google.maps.event.addListener(map, 'click', function () {
+        infowindow.close();
+    });
+
+    map.data.addListener('click', function (event) {
+        var category = event.feature.getProperty("category");
+        var skala = event.feature.getProperty("skala");
+        var place = event.feature.getProperty("place");
+        var url = event.feature.getProperty("url");
+        var status = event.feature.getProperty("status");
+        var sources = event.feature.getProperty("sources");
+
+         var featureData = {"category": category, "skala": skala,"place": place,"url": url,"status": status,"sources": sources,}
+         console.log(featureData);
+
+        infowindow.setContent(
+            '<div>' +
+                '<h3>' + category + '</h3>' +
+                '<p></p>' +
+                '<p><b>Ort:</b> ' + place + '<br /> ' +
+                '<b>Einstufung der Skala:</b> ' + skala + '<br /> ' +
+                '<b>Aktueller Status:</b> ' + status + '<br /> ' +
+                '<b>Informationsquelle:</b> ' + sources + '<br /> ' +
+                '<b>Link:</b> <a href=' + url + '>' + url + '</a> </p>' +
+            '</div>'
+
+        );
+        infowindow.setPosition(event.feature.getGeometry().get());
+        infowindow.setOptions({ pixelOffset: new google.maps.Size(0, -30) });
+        infowindow.open(map);
     });
 
     map.data.setStyle(function (feature) {
@@ -49,7 +80,8 @@ function initMap() {
             icon: getCircle(magnitude)
         };
     });
-}
+
+}//End Init Map
 
 function getCircle(magnitude) {
     return {
@@ -65,50 +97,6 @@ function getCircle(magnitude) {
 function eqfeed_callback(results) {
     map.data.addGeoJson(results);
 }
-
-
-
-//__________________________________________________________________
-
-// var contentString = '<div id="content">'+
-// '<div id="siteNotice">'+
-// '</div>'+
-// '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-// '<div id="bodyContent">'+
-// '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-// 'sandstone rock formation in the southern part of the '+
-// 'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-// 'south west of the nearest large town, Alice Springs; 450&#160;km '+
-// '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-// 'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-// 'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-// 'Aboriginal people of the area. It has many springs, waterholes, '+
-// 'rock caves and ancient paintings. Uluru is listed as a World '+
-// 'Heritage Site.</p>'+
-// '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-// 'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-// '(last visited June 22, 2009).</p>'+
-// '</div>'+
-// '</div>';
-
-// var infowindow = new google.maps.InfoWindow({
-// content: contentString
-// });
-
-// var marker = new google.maps.Marker({
-// position: uluru,
-// map: map,
-// title: 'Uluru (Ayers Rock)'
-// });
-// marker.addListener('click', function() {
-// infowindow.open(map, marker);
-// });
-
-
-
-//__________________________________________________________________
-
-
 
 
 
